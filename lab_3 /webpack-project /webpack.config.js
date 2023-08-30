47123b5289d8f847885cb251850cb2e6535937ad
DanilMalets <128975387+DanilMalets@users.noreply.github.com>
2023-08-30 18:39:43 +0300

    Update webpack.config.js

--- lab_3 /webpack-project /webpack.config.js
+++ lab_3 /webpack-project /webpack.config.js
@@ -1,34 +1,43 @@
-// webpack.config.js 
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = { 
- mode: 'none',
- entry: { 
- main: path.resolve(__dirname, './src/index.js'), 
- }, 
- output: { 
-    path: path.resolve(__dirname, './dist'), 
-    filename: '[name].bundle.js', 
- },
- plugins: [ 
-   new CleanWebpackPlugin(),
-   new HtmlWebpackPlugin({ 
-   title: 'webpack Boilerplate', 
-   template: path.resolve(__dirname, './src/pages/index.html'), 
-   filename: 'index.html',
-      },
-      {
-   title: 'webpack Boilerplate', 
-   template: path.resolve(__dirname, './src/pages/about.html'), 
-   filename: 'about.html', 
-      }), 
-   ],
-devServer: {
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Додано плагін
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/pages/index.html'),
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/pages/about.html'),
+      filename: 'about.html',
+    }),
+    new CopyWebpackPlugin({  // Додано цей блок для копіювання графічних матеріалів
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets', 'images'),
+          to: path.resolve(__dirname, 'dist', 'assets', 'images'),
+        },
+      ],
+    }),
+  ],
+  devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
     },
     compress: true,
     port: 9000,
   },
-} 
+};
